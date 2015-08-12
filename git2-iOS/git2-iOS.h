71d2cda3fba37a7739a95bae28d02a9bec97da2d
Ben Chatelain <benchatelain@gmail.com>
2015-08-12 10:14:22 -0600

    Initial Commit

---
+++ git2-iOS/git2-iOS.h
@@ -0,0 +1,19 @@
+//
+//  git2-iOS.h
+//  git2-iOS
+//
+//  Created by Ben Chatelain on 8/12/15.
+//  Copyright © 2015 phatblat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for git2-iOS.
+FOUNDATION_EXPORT double git2_iOSVersionNumber;
+
+//! Project version string for git2-iOS.
+FOUNDATION_EXPORT const unsigned char git2_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <git2_iOS/PublicHeader.h>
+
+
