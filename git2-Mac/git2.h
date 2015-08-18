5e5af51dbcdef14273516a17f87d2e09004699c1
Ben <ben@octop.ad>
2015-08-18 06:42:23 -0600

    Revert "Remove umbrella headers for now"
    This reverts commit 438352d112a18af740e02bb0715c9b848c82b34a.

---
+++ git2-Mac/git2.h
@@ -0,0 +1,19 @@
+//
+//  git2-Mac.h
+//  git2-Mac
+//
+//  Created by Ben Chatelain on 8/12/15.
+//  Copyright © 2015 phatblat. All rights reserved.
+//
+
+@import Cocoa;
+
+//! Project version number for git2-Mac.
+FOUNDATION_EXPORT double git2_MacVersionNumber;
+
+//! Project version string for git2-Mac.
+FOUNDATION_EXPORT const unsigned char git2_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <git2_Mac/PublicHeader.h>
+
+
