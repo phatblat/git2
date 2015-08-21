76d23aa382dba890ac4e81ad2922611d44215cda
Ben Chatelain <ben@octop.ad>
2015-08-21 14:40:39 -0600

    Fix mac scheme name

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -12,4 +12,4 @@ xcode-install install "7 beta 5"
 #carthage build --no-skip-current
 
 xcodebuild -workspace git2.xcworkspace -scheme git2-iOS -configuration Release build
-xcodebuild -workspace git2.xcworkspace -scheme git2-Mad -configuration Release build
+xcodebuild -workspace git2.xcworkspace -scheme git2-Mac -configuration Release build
