d5dda7d38892cef5982c556db20ff74fe7b7b311
Ben <ben@octop.ad>
2015-08-20 22:06:03 -0600

    Fix umbrella headers

--- git2-iOS/git2.h
+++ git2-iOS/git2.h
@@ -6,13 +6,11 @@
 //  Copyright © 2015 phatblat. All rights reserved.
 //
 
-@import UIKit;
+/// Project version number for git2-iOS.
+extern double git2_iOSVersionNumber;
 
-//! Project version number for git2-iOS.
-FOUNDATION_EXPORT double git2_iOSVersionNumber;
-
-//! Project version string for git2-iOS.
-FOUNDATION_EXPORT const unsigned char git2_iOSVersionString[];
+/// Project version string for git2-iOS.
+extern const unsigned char git2_iOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <git2_iOS/PublicHeader.h>
 
