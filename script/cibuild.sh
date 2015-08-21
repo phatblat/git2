f2385fd9367d2e4f6d521a8ffcd542e8e5bcb1ed
Ben Chatelain <benchatelain@gmail.com>
2015-08-21 14:19:21 -0600

    Add cibuild.sh

---
+++ script/cibuild.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+#
+# cibuild.sh
+# Builds the project, installing any necessary dependencies for the build server (TravisCI)
+#
+
+gem install xcode-install
+xcode-install list
+xcode-install install "7 beta 5"
+
+#carthage build --no-skip-current
+
+xcodebuild -workspace git2.xcworkspace -scheme git2-iOS -configuration Release build
+xcodebuild -workspace git2.xcworkspace -scheme git2-Mad -configuration Release build
